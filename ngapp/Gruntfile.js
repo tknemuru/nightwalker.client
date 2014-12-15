e4541de24eedfee4cc953162dd350c48904d8ed2
tknemuru <kojikoji.max.5252@gmail.com>
2014-12-16 05:15:48 +0900

    環境構築

--- ngapp/Gruntfile.js
+++ ngapp/Gruntfile.js
@@ -15,10 +15,14 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
+  // load the proxy
+  grunt.loadNpmTasks('grunt-connect-proxy');
+
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    //dist: 'dist'
+    dist: '../public'
   };
 
   // Define the configuration for all the tasks
