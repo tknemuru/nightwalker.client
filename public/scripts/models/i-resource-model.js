5b004e1ea5b31c40c1849da5e6e67f0de6adc6ec
tknemuru <kojikoji.max.5252@gmail.com>
2014-12-22 09:07:05 +0900

    publicを管理対象から外す

--- public/scripts/models/i-resource-model.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
-var NightWalker;
-(function (NightWalker) {
-    var Models;
-    (function (Models) {
-        "use strict";
-    })(Models = NightWalker.Models || (NightWalker.Models = {}));
-})(NightWalker || (NightWalker = {}));
-//# sourceMappingURL=i-resource-model.js.map
